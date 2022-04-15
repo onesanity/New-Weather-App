e8d049e39b58cc9a8106d66d233f2fba9d48c897
onesanity <98060550+onesanity@users.noreply.github.com>
2022-04-15 11:57:31 -0700

    remove console.log response

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
