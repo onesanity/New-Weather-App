8b11f1e8f6fdeab55dea8d108dd7d41f941a14e3
onesanity <98060550+onesanity@users.noreply.github.com>
2022-04-17 12:33:47 -0700

    created forecast JS template with loop

--- src/app.js
+++ src/app.js
@@ -21,6 +21,45 @@ function formatDate(timestamp) {
   return `: ${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Mon", "Tues", "Wed", "Thur","Fri"];
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function(day) {
+
+    forecastHTML = 
+      forecastHTML + 
+      `
+        <div class="col-2">
+          <div class="weather-forecast-date">
+            ${day}
+            </div>
+              <img 
+              src="http://openweathermap.org/img/wn/02d@2x.png" 
+              alt="" 
+              width="40px"
+              />
+              <div class="weather-forecast-temp">
+              <span class="weather-forecast-temp-max">
+                15°
+              </span>
+              <span class="weather-forecast-temp-min">
+                12°
+              </span>
+            </div>
+            </div>
+
+  `;
+  });
+
+forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+
+}
+
+
 function displayTemperature(response) {
   
   let temperatureElement = document.querySelector("#current-temp");
@@ -78,6 +117,8 @@ function displayCelsiusTemp(event) {
 
 let celsiusTemp = null;
 
+
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -86,4 +127,7 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+displayForecast();
 search("Bentonville");
+
