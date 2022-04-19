98fa06315cdaf21213ed94aa8f144b6d4785ce38
onesanity <98060550+onesanity@users.noreply.github.com>
2022-04-18 21:57:11 -0700

    forecast API integration, included current index, icon name,  rounded min and max temp, included if statement for index less than 6

--- src/app.js
+++ src/app.js
@@ -21,38 +21,47 @@ function formatDate(timestamp) {
   return `: ${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+  
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Mon", "Tues", "Wed", "Thur","Fri"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function(day) {
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
     forecastHTML = 
       forecastHTML + 
       `
         <div class="col-2">
-          <div class="weather-forecast-date">
-            ${day}
-            </div>
-              <img 
-              src="http://openweathermap.org/img/wn/02d@2x.png" 
+          <div class="weather-forecast-date">${formatDay
+            (forecastDay.dt)}</div>
+            <img 
+              src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
               alt="" 
               width="40px"
               />
               <div class="weather-forecast-temp">
               <span class="weather-forecast-temp-max">
-                15°
+                ${Math.round(
+                  forecastDay.temp.max)}°
               </span>
               <span class="weather-forecast-temp-min">
-                12°
+              ${Math.round(
+                forecastDay.temp.min)}°
               </span>
             </div>
             </div>
 
-  `;
+  `;}
   });
 
 forecastHTML = forecastHTML + `</div>`;
